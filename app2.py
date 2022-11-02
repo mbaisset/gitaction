9f065231959c9b4723329d89d270300a20e50b52
Ubuntu <sg@devopssg.5kzjnz3spbnungat25ddsebbbb.ax.internal.cloudapp.net>
2022-11-02 10:26:38 +0000

    Modification of app2.py

--- app2.py
+++ app2.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World modified!"
+    return "Hello, World modified! in actions"
 
 if __name__ == "__main__":
     app.run("0.0.0.0",5003)
