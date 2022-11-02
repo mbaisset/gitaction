66296b03be7e27216afddc7c323ff1c9b8ecfd2a
root <root@LAPTOP-DONPCONR.localdomain>
2022-11-02 19:18:33 +0100

    Adding log when launching moyenne

--- moyenne.py
+++ moyenne.py
@@ -1,2 +1,12 @@
+from datetime import datetime
+def generate_log(params):
+
+        with open('logs.txt', 'a') as f:
+
+                    f.write('on a appelé la fonction moyenne {} avec les parametres {}\n'.format(
+                                datetime.now(), params)
+                                        )
+
 def moyenne (data):
+    generate_log (data)
     return sum(data)/len(data)
